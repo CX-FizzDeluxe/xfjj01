796ad7dfd063ed8fe556f5cf550de692b31c9426
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-18 16:48:45 +0000

    Mon Dec 18 16:48:45 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -59,6 +59,39 @@ public class FizzBuzz {
 	 * @param number
 	 * @return "fizz" or "buzz" or "fizz buzz" or number
 	 */
+	/*public static String fizz_Buzz(Integer number) {
+
+		StringBuilder result = new StringBuilder();
+		Boolean isRes = false;
+
+		if (number % THREE_INT == 0 || number.toString().indexOf(THREE_STR) >= 0) {
+			result.append(FIZZ);
+			isRes = true;
+		}
+		if (number % FIVE_INT == 0 || number.toString().indexOf(FIVE_STR) >= 0) {
+			if (isRes) {
+				result.append(SPACE);
+			}
+			result.append(BUZZ);
+			isRes = true;
+		}
+
+		if (!isRes.booleanValue()) {
+			result.append(number.intValue());
+		}
+
+		return result.toString();
+	}*/
+	
+	/**
+	 * This method returns below: "fizz" if number is divisible by 3 or if it
+	 * has a 3 in it "buzz" if number is divisible by 5 or if it has a 5 in it
+	 * if number is both "fizz" and "buzz" at the same time. If this happens
+	 * then returns "fizz buzz"
+	 * 
+	 * @param number
+	 * @return "fizz" or "buzz" or "fizz buzz" or number
+	 */
 	public static String fizz_Buzz(Integer number) {
 
 		StringBuilder result = new StringBuilder();
