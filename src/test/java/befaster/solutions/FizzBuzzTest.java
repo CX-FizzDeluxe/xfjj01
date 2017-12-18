c4f3c19fd87200b572dd2264322cf912c874f2d0
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-18 16:13:32 +0000

    Mon Dec 18 16:13:32 GMT 2017

--- src/test/java/befaster/solutions/FizzBuzzTest.java
+++ src/test/java/befaster/solutions/FizzBuzzTest.java
@@ -1,10 +1,13 @@
 package befaster.solutions;
 
-import org.junit.Test;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class FizzBuzzTest {
 
     @Test
